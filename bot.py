64dd05bb37ac216cc38087d1a5d0a636f5aecf11
Volog122 <86968036+Volog122@users.noreply.github.com>
2025-05-25 19:51:08 +0500

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,29 @@
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
+from telegram.ext import ApplicationBuilder, CommandHandler
+
+
+async def start(update: Update, context):
+    keyboard = [
+        [
+            InlineKeyboardButton(
+                "🛍️ Открыть Bazaar",
+                web_app=WebAppInfo(
+                    url="https://https-github-com-yourusername-telegram-s1o7.onrender.com/"
+                ),
+            )
+        ]
+    ]
+    await update.message.reply_text(
+        "🛒 Добро пожаловать!", reply_markup=InlineKeyboardMarkup(keyboard)
+    )
+
+
+if __name__ == "__main__":
+    application = (
+        ApplicationBuilder()
+        .token("8090139284:AAE4gDvRE8SopVouz8SNeeOmQduy4aXiOPw")
+        .build()
+    )
+    application.add_handler(CommandHandler("start", start))
+    print("Бот запущен...")
+    application.run_polling()
